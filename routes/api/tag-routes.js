276d88d82331f8c68ae093056a9ee039defed445
Anjeh Mbah <junez24@yahoo.com>
2021-08-04 14:09:34 -0400

    fixing tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,10 +64,42 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+        id: req.params.id
+    }
+})
+    .then(responseData => {
+        if (!responseData[0]) {
+            res.status(404).json({ message: 'No tag found with this id' });
+            return;
+        }
+        res.json(responseData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+        id: req.params.id
+    }
+})
+    .then(responseData => {
+        if (!responseData) {
+            res.status(404).json({ message: 'No tag found with this id' });
+            return;
+        }
+        res.json(responseData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
